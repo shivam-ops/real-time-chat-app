7fa6fc63d8b952432855886d8085973743ddd505
Shivam Joshi <shivamjoshi0822@gmail.com>
2020-09-04 11:26:49 +0530

    creating chat component and connecting front with backend using ssocket

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+let socket;
 
 
-const Chat = () => {
+const Chat = ({location}) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = `localhost:5000`;
+
+    useEffect(() => {
+        const {name, room} = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+       
+        setName(name);
+        setRoom(room); 
+
+        socket.emit('join', {name, room});
+    }, [ENDPOINT, location.search])
+
     return (
         <h1>Chat</h1>
     );
